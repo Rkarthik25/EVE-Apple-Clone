dbedbc9c16d6584942f93ba8903ea51f2899e2c9
Monojit Mondal <mondalmonojit1696@gmail.com>
2023-02-25 03:08:35 +0530

    fw19_0306: updated complete folder structure with readme

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import MainRoute from "./routes/MainRoute";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <MainRoute />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
