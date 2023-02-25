e110557a5146de1bf813a8b24b426c9fec965bbc
Prabal <prabal19999@gmail.com>
2023-02-25 17:46:46 +0530

    home page

--- admin-panel/FrontEnd/components/StatsHome.tsx
+++ admin-panel/FrontEnd/components/StatsHome.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
-
+import { Box,Text,Grid, GridItem } from '@chakra-ui/react'
 const StatsHome = () => {
-  
+return <Grid templateColumns='repeat(5, 1fr)' gap={10} as="b" maxH={20} w="">
+  <Box textAlign={"center"} bg='blue.500'>
+    <Text>Total Users</Text>
+    <Text>16</Text>
+    </Box>
+    <Box textAlign={"center"} bg='red.500'>
+    <Text>Total Admins</Text>
+    <Text>16</Text>
+    </Box>
+    <Box textAlign={"center"} bg='green.500'>
+    <Text>Total Items</Text>
+    <Text>160</Text>
+    </Box>
+    <Box textAlign={"center"} bg='pink.500'>
+    <Text>Total Users</Text>
+    <Text>16</Text>
+    </Box>
+    <Box textAlign={"center"} bg='yellow.500'>
+    <Text>Total Users</Text>
+    <Text>16</Text>
+  </Box>
+</Grid>
+
 }
 
 export default StatsHome
