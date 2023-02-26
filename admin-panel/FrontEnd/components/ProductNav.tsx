40ef62b7196fb062e2fd91eea902728fd96d3305
Prabal <prabal19999@gmail.com>
2023-02-27 03:29:08 +0530

    pushed the final front end code of admin panel

---
+++ admin-panel/FrontEnd/components/ProductNav.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import axios from 'axios';
+import { Select,Box, Button } from "@chakra-ui/react";
+import Add_Modal from "../components/AddModal"
+const ProductNav = (props) => {
+const [order,setOrder]= React.useState(1)
+const [Category,setCategory]= React.useState(null)
+
+const FilterByCategory=async(Category:string)=>{
+  
+  let data=  await axios.get(`http://localhost:4500/product?Category=${Category}`)
+props.onData(data.data)
+  console.log(data)
+}
+
+
+const SortByPrice=async(order)=>{
+    if(Category){
+    let data= await axios.get(`http://localhost:4500/product?Category=${Category}&PriceToAccess=${+order}`)
+    props.onData(data.data)
+    }
+    else{
+        let data= await axios.get(`http://localhost:4500/product?PriceToAccess=${+order}`)
+        props.onData(data.data)
+    }
+
+}
+
+    const  DropdownForCategory=() =>{
+        return (
+          <Select onChange={(e)=>{
+         
+            FilterByCategory(e.target.value)
+            setCategory(e.target.value)
+       
+        }} placeholder="Select a Category">
+            <option value="Mac">Mac</option>
+            <option value="Iphone">iPhone</option>
+            <option value="Ipad">iPad</option>
+            <option value="Iwatch">iWatch</option>
+            <option value="HomePod">HomePod</option>
+            <option value="AppleTv">Apple TV</option>
+            <option value="AirPods">Air Pods</option>
+          </Select>
+        );
+    }
+        const  DropdownForPrice=() =>{
+            return (
+              <Select onChange={(e)=>SortByPrice(e.target.value)} placeholder="Sort by Price">
+                <option value="1">low to high</option>
+                <option value="-1">high to low</option>
+              </Select>
+            );
+      }
+  return (
+  <Box display={"flex"} justifyContent={"space-around"}>
+<Box display={"flex"} w="40%">
+{DropdownForCategory()}
+{DropdownForPrice()}
+</Box>
+
+<Add_Modal/>
+
+  </Box>
+  )
+}
+
+export default ProductNav
---
+++ admin-panel/FrontEnd/src/pages/users.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import axios from 'axios'
+import SingleRowUsers from "../../components/SingleRowUsers"
+import SignIn from 'components/SignIn'
+import Sidebar from 'components/Sidebar'
+import {
+    Table,
+    Thead,
+    Tbody,
+   
+    Tr,
+    Th,
+    Box
+  
+  } from '@chakra-ui/react'
+  import { AuthContext } from 'context/authContext';
+import { useContext } from 'react';
+
+const Users = () => {
+    const [users,setUsers]= React.useState([])
+     const {name}= useContext(AuthContext)
+    const getUsers=async ()=>{
+      let users=await  axios.get("http://localhost:4500/users")
+     setUsers(users.data)
+
+    }
+  
+    React.useEffect(()=>{
+getUsers()
+    },[])
+  return (
+    <Box display={"flex"}>
+<Sidebar />
+{!name?.length ? <SignIn/> :
+  <Box>
+<Table>
+    <Thead>
+        <Tr>
+            <Th>Name</Th>
+            <Th>Email</Th>
+            <Th>Mobile </Th>
+            <Th>DOB</Th>
+            <Th>Item in the Cart</Th>
+        </Tr>
+    </Thead>
+    <Tbody>
+        {
+             users.map((el)=><SingleRowUsers key={el._id} Name={el.Fname} email={el.email}  mobile={el.phone} DOB={el.DOB} cart={el.cart.length}/>)
+        }
+    </Tbody>
+</Table>
+</Box>
+}
+</Box>
+  )
+}
+
+export default Users
