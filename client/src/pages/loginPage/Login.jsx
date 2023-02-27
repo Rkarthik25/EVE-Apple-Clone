0db4555f47e239d9a3ed6e9420c3c925a96df6b9
Monojit Mondal <mondalmonojit1696@gmail.com>
2023-02-27 08:38:29 +0530

    fw19_0306: Backend deployed URL merged to frontend

--- client/src/pages/loginPage/Login.jsx
+++ client/src/pages/loginPage/Login.jsx
@@ -1,108 +1,65 @@
-
-import { Box,useToast, Button, Heading, Input, Link, Text } from '@chakra-ui/react'
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { login } from '../../redux/authReducer/action';
+import {
+  Box,
+  useToast,
+  Button,
+  Heading,
+  Input,
+  Link,
+  Text,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-
-
-
 export const Login = () => {
-
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
- // const [show, setShow] = React.useState(false);
-
-  const dispatch = useDispatch();
   const navigate = useNavigate();
   const toast = useToast();
 
-  const token = useSelector(
-    (state) => state.authReducer.isAuth
-  );
-  //console.log(token)
+  const getdata = () => {
+    const data = {
+      email,
+      password,
+    };
 
- // const handlePass = () => setShow(!show);
+    return axios
+      .post(
+        "https://friendly-fawn-pocketbook.cyclic.app/users/user/login",
+        data
+      )
+      .then((res) => {
+        console.log(res.data);
+        console.log(res.data.usertoken);
+        localStorage.setItem("userID", res.data?.id);
 
-const getdata = ()=>{
-  const data = {
-    email,
-    password,
+        return res.data.usertoken;
+      });
   };
 
-  return axios
-  .post("http://localhost:4500/users/user/login", data)
-  .then((res) => {
-    console.log(res.data)
-     console.log(res.data.usertoken)
-      localStorage.setItem("userID", res.data?.id)
-  
-      return res.data.usertoken;
-  });
+  const handleClick = async () => {
+    if (email && password) {
+      const token = await getdata();
+      console.log(token, "token");
 
-}
-
-  const handleClick = async() => {
-    // const data = {
-    //   email,
-    //   password,
-    // };
-     if (email && password) {
-
-  //  const token = await getdata();
-  const token = await getdata();
-  console.log(token,"token")
-  
-  if(token){
-    toast({
-               description: "Login success",
-               status: "success",
-               duration: 3000,
-              isClosable: true,
-            });
-            navigate("/")
-  }else{
-    toast({
-    description: "Wrong Credentials !",
-                    status: "warning",
-                 duration: 3000,
-                  isClosable: true,
-                 });
-                }
-
-
-     // dispatch(login(data));
-  //     if(token!==""){
-  //      
-  //  }else{
-  //    
-  //                 description: "Wrong Credentials !",
-  //                 status: "warning",
-  //              duration: 3000,
-  //               isClosable: true,
-  //               });
-  // }
-        // toast({
-        //   description: "Login success",
-        //   status: "success",
-        //   duration: 3000,
-        //   isClosable: true,
-        // });
-        // navigate("/Checkout");
-        // if (loginSuccess === "Wrong Credentials !") {
-        //          toast({
-        //           description: "Wrong Credentials !",
-        //           status: "warning",
-        //         duration: 3000,
-        //           isClosable: true,
-        //         });
-            //  }
-    }
-     else {
+      if (token) {
+        toast({
+          description: "Login success",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
+        navigate("/");
+      } else {
+        toast({
+          description: "Wrong Credentials !",
+          status: "warning",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+    } else {
       toast({
         description: "Please fill the required details",
         status: "info",
@@ -110,67 +67,42 @@ const getdata = ()=>{
         isClosable: true,
       });
     }
-   
   };
-  
-  
 
-//  // if (loginSuccess !== "") {
-//     if (loginSuccess === "Wrong Credentials !") {
-//       toast({
-//         description: "Wrong Credentials !",
-//         status: "warning",
-//         duration: 3000,
-//         isClosable: true,
-//       });
-//     } 
-//     else if (loginSuccess === "Authentication Failed !") {
-//       toast({
-//         description: "Authentication Failed !",
-//         status: "warning",
-//         duration: 3000,
-//         isClosable: true,
-//       });
-//     } 
-    // else {
-    //   toast({
-    //     description: "login Successfully",
-    //     status: "success",
-    //     duration: 3000,
-    //     isClosable: true,
-    //   });
-    //   navigate("/");
-    // }
- // }
-
-  // if (loginFailure !== "") {
-  //   toast({
-  //     description: loginFailure,
-  //     status: "error",
-  //     duration: 4000,
-  //     isClosable: true,
-  //   });
-  // }
   return (
-    <Box mt="15px" ml="35%" mr="35%" w="30%" >
+    <Box mt="15px" ml="35%" mr="35%" w="30%">
       <Box>
-<Heading  >
-Sign in for faster checkout.
-</Heading>
-      </Box >
-
-      <Box mt={"60px"} w="80%" >
-        <Text mt="4px" mb="8px">
-        Sign in to EVE Store
-        </Text>
-        <Input type="text" mt="4px" value={email} onChange={(e)=> setEmail(e.target.value)} placeholder="EVE ID" />
-        <Input type = "password" mt="8px" value={password} placeholder="Password" onChange={(e)=> setPassword(e.target.value)} ></Input>
-        <Button onClick={handleClick} mt="14px" mb="4px">Signin</Button>
-    <hr style={{margin: "20px 0px 20px 0px", width:"100%" }} />
-      
-      <Text>Don't have an EVE ID? <span><Link color={"blue.500"} >Create yours now</Link></span></Text>
+        <Heading>Sign in for faster checkout.</Heading>
       </Box>
+      <Box mt={"60px"} w="80%">
+        <Text mt="4px" mb="8px">
+          Sign in to EVE Store
+        </Text>
+        <Input
+          type="text"
+          mt="4px"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="EVE ID"
+        />
+        <Input
+          type="password"
+          mt="8px"
+          value={password}
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}></Input>
+        <Button onClick={handleClick} mt="14px" mb="4px">
+          Signin
+        </Button>
+        <hr style={{ margin: "20px 0px 20px 0px", width: "100%" }} />
 
-    </Box>
-  )
-}
+        <Text>
+          Don't have an EVE ID?{" "}
+          <span>
+            <Link color={"blue.500"}>Create yours now</Link>
+          </span>
+        </Text>
+      </Box>
+    </Box>
+  );
+};
